1343ad635943a3e4bb181014fecd04149ddc9a1c
Pramish <pramish.luitel7@gmail.com>
2021-03-06 18:44:12 +1100

    ADD Readme for coding challenge

--- Week1/CodingTest.js
+++ Week1/CodingTest.js
@@ -3,15 +3,20 @@
  * Question:
  *  Part One: Write a program to remove a specific character of a given string and return the new string.
  
- *  Part Two: After getting a new String, Write a program to check whether a given new string represents a correct sentence or not. A string is considered a correct sentence if it starts with the capital letters and ends with a full stop (.)
+ *  Part Two: After getting a new String, Write a program to check whether a given new string represents a correct sentence or not. A string is considered a correct sentence if it starts with the capital letters and ends with a full stop (.). Also return the correct sentence.
+ 
+ *  Part Three: After checking if the sentence is a complete sentence, count the number of repetitive words in that sentence.
  */
+
 class RemoveCheckString {
   /** ToDo **/
+
   removeString(stringToRemove, mySentence) {
     // return string
   }
   checkCompleteSentence(mySentence) {
     // return boolean
   }
+  /** ToDo */
 }
 const result = new RemoveCheckString();
---
+++ Week1/README.md
@@ -0,0 +1,3 @@
+## How to run the file
+
+1. Simply run node [file_name]. Make sure you have node installed on your machine
