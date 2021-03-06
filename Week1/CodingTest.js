26a6614eca44bd0f12189d0856f554cf023cddce
Pramish <pramish.luitel7@gmail.com>
2021-03-06 18:41:51 +1100

    Add a coding question.

---
+++ Week1/CodingTest.js
@@ -0,0 +1,17 @@
+/**
+ * Leverage the power of Classes and Objects, please solve the following problem.
+ * Question:
+ *  Part One: Write a program to remove a specific character of a given string and return the new string.
+ 
+ *  Part Two: After getting a new String, Write a program to check whether a given new string represents a correct sentence or not. A string is considered a correct sentence if it starts with the capital letters and ends with a full stop (.)
+ */
+class RemoveCheckString {
+  /** ToDo **/
+  removeString(stringToRemove, mySentence) {
+    // return string
+  }
+  checkCompleteSentence(mySentence) {
+    // return boolean
+  }
+}
+const result = new RemoveCheckString();
